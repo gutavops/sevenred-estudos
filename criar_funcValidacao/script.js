5a8b6028e8637eb68ddb3b9467c806447e654ff7
Gustavo Silva <gustavops1985@gmail.com>
2025-02-25 18:06:13 -0300

    cria funcValidacao

---
+++ criar_funcValidacao/script.js
@@ -0,0 +1,13 @@
+function calculadora(number1, number2) {
+    let soma = number1 + number2;
+
+    if (soma > 10) {
+        console.log("Maior que 10");
+    } else if (soma < 10) {
+        console.log("Menor que 10");
+    } else {
+        console.log("Igual a 10");
+    }
+}
+
+calculadora(5, 6)
