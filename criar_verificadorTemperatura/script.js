6592eaf9328867fbd6fa8b6ac520f45353e430de
Gustavo Silva <gustavops1985@gmail.com>
2025-02-25 18:06:42 -0300

    cria verificadorTemperatura

---
+++ criar_verificadorTemperatura/script.js
@@ -0,0 +1,10 @@
+function verificarTemperatura(temperatura) {
+    if (temperatura > 25) {
+        console.log("Está quente! Ligando o ar-condicionado.");
+    } else {
+        console.log("Está frio. Não precisa ligar o ar-condicionado.");
+    }
+}
+
+verificarTemperatura(30); 
+verificarTemperatura(20); 
