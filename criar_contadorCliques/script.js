18b4bf33a8f61292f2b2b8f7ece73a44002d4048
Gustavo Silva <gustavops1985@gmail.com>
2025-03-24 18:15:11 -0300

    add localStorage

--- criar_contadorCliques/script.js
+++ criar_contadorCliques/script.js
@@ -1,18 +1,28 @@
+console.log(typeof localStorage.getItem("contador"))
 // Variável que armazena o valor do contador
-let count = 0;
+let count = localStorage.getItem("contador")|| 0;
+
+updateCounter(); // Atualiza a tela ao abrir a página
+
+// Primeiro passo meu foi criar uma função que salva o valor do contador
+function salvarLocalStorage() {
+    localStorage.setItem("contador", count);
+}
 
 // Função que adiciona +1 na variável
 function increaseCount() {
     count++; // Incrementa 1
     updateCounter() // Atualiza o valor na tela
-    console.log("Depois de increaseCount():", count);
+    console.log("Depois de increaseCount():", count)
+    salvarLocalStorage()
 }
 
 // Função que diminui -1 na variável
 function decreaseCount() {
     count--; // Decrementa 1
     updateCounter() // Atualiza o valor na tela
-    console.log("Depois de decreaseCount():", count);
+    console.log("Depois de decreaseCount():", count)
+    salvarLocalStorage()
 }
 
 function updateCounter() {
