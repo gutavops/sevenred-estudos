1e0f4b2ecf5e47dc2a3780af281e7986ae6d5021
Gustavo Silva <gustavops1985@gmail.com>
2025-05-22 10:49:03 -0300

    add theme

--- React/criar_contadorCliqueReact/src/App.tsx
+++ React/criar_contadorCliqueReact/src/App.tsx
@@ -1,14 +1,34 @@
 import { useState } from 'react'
+import { MoonIcon, SunIcon} from '@phosphor-icons/react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState<'light' | 'dark'>('light')
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'dark' ? 'light' : 'dark'
+    setTheme(newTheme)
+    
+      document.body.classList.toggle('dark')
+    
+  }
+  
   return (
-    <>  
-      <h1>Contador: {count}</h1>
+    <>
+      <header>  
+        <h1>Contador: {count}</h1>
+        <div className='theme'>
+          <button id='toggle' onClick={toggleTheme}>
+            {theme == 'dark' ? <MoonIcon size={24}/> : <SunIcon size={24}/>}
+          </button>
+        </div>
+      </header>
+      <div className='countButton'>
         <button onClick={() => setCount((count) => count + 1)}>
           Clique
         </button>
+      </div>
     </>
   )
 }
